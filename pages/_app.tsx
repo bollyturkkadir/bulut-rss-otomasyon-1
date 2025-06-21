c856453d73f1f14b15b49bbd1f0b77da423134ff
Kadir <abdulkadirhanturker@gmail.com>
2025-06-21 23:36:28 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,7 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
-// AuthProvider import'u ve kullanımı tamamen kaldırıldı.
-
 function MyApp({ Component, pageProps }: AppProps) {
-  // Artık AuthProvider ile sarmalama yapmıyoruz.
   return <Component {...pageProps} />;
 }
 
